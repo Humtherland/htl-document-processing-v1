568e35e891c1b2a19f38394725d5f95cb33f5a87
samuel14luis <luismendoza.samuel@gmail.com>
2023-05-30 16:16:15 -0500

    feat: scaffolding and prepare module for documents management

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DocumentModule } from './document/document.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    DocumentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
